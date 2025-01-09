0ee252b224fb4a637ea0bc90b12b9e209d993828
Adriniks <alinabipourboy77@gmail.com>
2025-01-09 21:35:17 +0330

    Add files via upload

--- server.js
+++ server.js
@@ -96,6 +96,6 @@ app.post("/accept-request", async (req, res) => {
 });
 
 // شروع سرور
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(1000, () => {
+  console.log("Server is running on http://localhost:1000");
 });
