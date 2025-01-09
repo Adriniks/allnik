a4e6f5e70f34c8abf638ff166c704e878f5ee56b
Adriniks <alinabipourboy77@gmail.com>
2025-01-09 08:16:51 +0330

    Add files via upload

--- server.js
+++ server.js
@@ -208,5 +208,5 @@ app.get('/api/users/me', authMiddleware, async (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
